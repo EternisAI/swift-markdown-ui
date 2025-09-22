b5288fd59d5f46b0337f4aa5be339801b05af1ef
Joel Drotleff <joel.drotleff@me.com>
2025-09-22 09:50:58 -0700

    swiftformat

--- Sources/MarkdownUI/Utility/InlineNode+PlainText.swift
+++ Sources/MarkdownUI/Utility/InlineNode+PlainText.swift
@@ -1,23 +1,23 @@
 import Foundation
 
 extension Sequence where Element == InlineNode {
-  func renderPlainText() -> String {
-    self.collect { inline in
-      switch inline {
-      case .text(let content):
-        return [content]
-      case .softBreak:
-        return [" "]
-      case .lineBreak:
-        return ["\n"]
-      case .code(let content):
-        return [content]
-      case .html(let content):
-        return [content]
-      default:
-        return []
-      }
+    func renderPlainText() -> String {
+        collect { inline in
+            switch inline {
+            case let .text(content):
+                return [content]
+            case .softBreak:
+                return [" "]
+            case .lineBreak:
+                return ["\n"]
+            case let .code(content):
+                return [content]
+            case let .html(content):
+                return [content]
+            default:
+                return []
+            }
+        }
+        .joined()
     }
-    .joined()
-  }
 }
